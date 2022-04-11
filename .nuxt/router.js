424205508b9ba235bd82e47ac23adaf4c6b93919
josemiguel_chavezg <josemiguel@aureolab.cl>
2022-04-11 18:00:11 -0400

    ignorando

---
+++ .nuxt/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _328fd13a = () => interopDefault(import('..\\pages\\crear.vue' /* webpackChunkName: "pages/crear" */))
+const _5b0c2dba = () => interopDefault(import('..\\pages\\listar.vue' /* webpackChunkName: "pages/listar" */))
+const _d9e09ba4 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/crear",
+    component: _328fd13a,
+    name: "crear"
+  }, {
+    path: "/listar",
+    component: _5b0c2dba,
+    name: "listar"
+  }, {
+    path: "/",
+    component: _d9e09ba4,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
